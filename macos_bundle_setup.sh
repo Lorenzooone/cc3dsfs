1bb8e16b238ee5a1df640cb54d0fa0f6155a7e42
Lorenzooone <lollo.lollo.rbiz@gmail.com>
2024-05-28 21:58:19 +0200

    A

--- macos_bundle_setup.sh
+++ macos_bundle_setup.sh
@@ -1,12 +1,13 @@
 framework_change_pos() {
+	echo "install_name_tool -change @rpath/../Frameworks/${2}.framework/Versions/A/${2} @executable_path/../Frameworks/${2}.framework/Versions/A/${2} ${1}"
     install_name_tool -change @rpath/../Frameworks/${2}.framework/Versions/A/${2} @executable_path/../Frameworks/${2}.framework/Versions/A/${2} ${1}
 }
 
-framework_change_pos ${1} "freetype"
-framework_change_pos ${1} "OpenAL"
-framework_change_pos ${1} "vorbisenc"
-framework_change_pos ${1} "vorbisfile"
-framework_change_pos ${1} "FLAC"
-framework_change_pos ${1} "vorbis"
-framework_change_pos ${1} "ogg"
+framework_change_pos ${1} 'freetype'
+framework_change_pos ${1} 'OpenAL'
+framework_change_pos ${1} 'vorbisenc'
+framework_change_pos ${1} 'vorbisfile'
+framework_change_pos ${1} 'FLAC'
+framework_change_pos ${1} 'vorbis'
+framework_change_pos ${1} 'ogg'
 
