0c670ba712d3c470622e1ddcc5a1303f3f86317b
Lorenzooone <lollo.lollo.rbiz@gmail.com>
2025-09-26 23:10:22 +0200

    Simplify Raspberry Pi changing chosen script

---
+++ rpi_setup/files/Desktop/cc3dsfs_script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+TARGET_SCRIPT=cc3dsfs_script_tv_kit_2.sh
+
+BASE_DIR=${HOME}/Desktop/premade_scripts
+
+${BASE_DIR}/${TARGET_SCRIPT}
--- rpi_setup/setup.sh
+++ rpi_setup/setup.sh
@@ -6,7 +6,8 @@ if [ "$(id -u)" -eq "0" ]; then
 fi
 
 PROGRAM_NAME=cc3dsfs
-DEFAULT_SCRIPT_KIOSK_MODE=${PROGRAM_NAME}_script_tv_kit_2.sh
+BASE_SOURCE_DIR="."
+BASE_TARGET_DIR=${HOME}
 
 sudo apt update
 sudo apt -y install xterm gpiod xserver-xorg xinit libxcursor1 x11-xserver-utils pipewire pipewire-alsa libharfbuzz-icu0
@@ -21,24 +22,24 @@ sudo systemctl disable bluetooth.service
 
 # I know this isn't how you SHOULD do it, however I tried
 # various other things (crontab @reboot and rc.local) and they didn't work...
-echo "" >> ${HOME}/.bashrc
-echo 'if [ -z "${EXECUTED_ONCE}" ]; then' >> ${HOME}/.bashrc
-echo "  export EXECUTED_ONCE=1 ; startx ${HOME}/Desktop/${PROGRAM_NAME}_script.sh" >> ${HOME}/.bashrc
-echo 'fi' >> ${HOME}/.bashrc
+echo "" >> ${BASE_TARGET_DIR}/.bashrc
+echo 'if [ -z "${EXECUTED_ONCE}" ]; then' >> ${BASE_TARGET_DIR}/.bashrc
+echo "  export EXECUTED_ONCE=1 ; startx ${BASE_TARGET_DIR}/Desktop/${PROGRAM_NAME}_script.sh" >> ${BASE_TARGET_DIR}/.bashrc
+echo 'fi' >> ${BASE_TARGET_DIR}/.bashrc
 
-cp -Rf files/Desktop/ ${HOME}/
+rm -f ${BASE_TARGET_DIR}/Desktop/${PROGRAM_NAME}_script.sh
+cp -Rf ${BASE_SOURCE_DIR}/files/Desktop/ ${BASE_TARGET_DIR}/
 if [ $(getconf LONG_BIT) -eq 32 ]; then
-  $(cd ${HOME}/Desktop ; rm -f ${PROGRAM_NAME} ; ln -s ${PROGRAM_NAME}_versions/${PROGRAM_NAME}_32 ${PROGRAM_NAME})
+  $(cd ${BASE_TARGET_DIR}/Desktop ; rm -f ${PROGRAM_NAME} ; ln -s ${PROGRAM_NAME}_versions/${PROGRAM_NAME}_32 ${PROGRAM_NAME})
 else
-  $(cd ${HOME}/Desktop ; rm -f ${PROGRAM_NAME} ; ln -s ${PROGRAM_NAME}_versions/${PROGRAM_NAME}_64 ${PROGRAM_NAME})
+  $(cd ${BASE_TARGET_DIR}/Desktop ; rm -f ${PROGRAM_NAME} ; ln -s ${PROGRAM_NAME}_versions/${PROGRAM_NAME}_64 ${PROGRAM_NAME})
 fi
-$(cd ${HOME}/Desktop ; rm -f ${PROGRAM_NAME}_script.sh ; ln -s premade_scripts/${DEFAULT_SCRIPT_KIOSK_MODE} ${PROGRAM_NAME}_script.sh)
-chmod +x -R ${HOME}/Desktop/premade_scripts/
-chmod +x -R ${HOME}/Desktop/${PROGRAM_NAME}_versions/
-chmod +x ${HOME}/Desktop/${PROGRAM_NAME}
-chmod +x ${HOME}/Desktop/${PROGRAM_NAME}_script.sh
-chmod +x ${HOME}/Desktop/update_to_unstable.sh
-chmod +x ${HOME}/Desktop/update_to_stable.sh
+chmod +x -R ${BASE_TARGET_DIR}/Desktop/premade_scripts/
+chmod +x -R ${BASE_TARGET_DIR}/Desktop/${PROGRAM_NAME}_versions/
+chmod +x ${BASE_TARGET_DIR}/Desktop/${PROGRAM_NAME}
+chmod +x ${BASE_TARGET_DIR}/Desktop/${PROGRAM_NAME}_script.sh
+chmod +x ${BASE_TARGET_DIR}/Desktop/update_to_unstable.sh
+chmod +x ${BASE_TARGET_DIR}/Desktop/update_to_stable.sh
 
 if [ -e /boot/firmware/config.txt ]; then
   FIRMWARE=/firmware
@@ -46,10 +47,10 @@ else
   FIRMWARE=
 fi
 
-sudo cp -f files/config.txt /boot${FIRMWARE}/config.txt
+sudo cp -f ${BASE_SOURCE_DIR}/files/config.txt /boot${FIRMWARE}/config.txt
 
-sudo cp -Rf files/usr/ /
-sudo cp -Rf files/etc/ /
+sudo cp -Rf ${BASE_SOURCE_DIR}/files/usr/ /
+sudo cp -Rf ${BASE_SOURCE_DIR}/files/etc/ /
 sudo chmod +x /etc/rc.local
 
 sleep 5
