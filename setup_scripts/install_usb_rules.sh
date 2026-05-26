45a637034aaf11cb9ed6e81f832dbf2b9e7cf983
Lorenzooone <lollo.lollo.rbiz@gmail.com>
2026-05-26 04:13:25 +0200

    Improve rules installation script for Linux

--- setup_scripts/install_usb_rules.sh
+++ setup_scripts/install_usb_rules.sh
@@ -1,3 +1,27 @@
-cp *.rules /etc/udev/rules.d/
+#!/bin/sh
+
+if [ "$(id -u)" -ne "0" ]; then
+	echo "This script shoud be run as root!";
+	exit 1;
+fi
+
+cd -P -- "$(dirname -- "$0")"
+
+# Default...
+RULES_DIR=.
+
+if [ -d "usb_rules" ]; then
+	# Handle releases
+	RULES_DIR=usb_rules
+elif [ -d "../usb_rules" ]; then
+	# Handle people self-compiling
+	RULES_DIR=../usb_rules
+fi
+
+REAL_PATH_RULES=$(realpath $RULES_DIR)
+echo "Looking inside $REAL_PATH_RULES for *.rules files..."
+
+cp -vf $RULES_DIR/*.rules /etc/udev/rules.d/
+
 udevadm control --reload-rules
 udevadm trigger
--- setup_scripts/rpi_mono_setup_setup.sh
+++ setup_scripts/rpi_mono_setup_setup.sh
@@ -10,7 +10,7 @@ mkdir -p ${RPI_SETUP_FOLDER}/files/usr/lib/udev/rules.d
 cp ${EXTRACTION_FOLDER}32/cc3dsfs ${MAIN_FILES_RPI_SETUP}/cc3dsfs_versions/cc3dsfs_32
 cp ${EXTRACTION_FOLDER}64/cc3dsfs ${MAIN_FILES_RPI_SETUP}/cc3dsfs_versions/cc3dsfs_64
 cp -r "${EXTRACTION_FOLDER}32/other licenses" "${MAIN_FILES_RPI_SETUP}/other licenses"
-cp ${EXTRACTION_FOLDER}32/*.rules ${RPI_SETUP_FOLDER}/files/usr/lib/udev/rules.d
+cp ${EXTRACTION_FOLDER}32/usb_rules/*.rules ${RPI_SETUP_FOLDER}/files/usr/lib/udev/rules.d
 cp ${EXTRACTION_FOLDER}32/LICENSE ${MAIN_FILES_RPI_SETUP}
 cp ${EXTRACTION_FOLDER}32/README.md ${MAIN_FILES_RPI_SETUP}
 $(cd ${RPI_SETUP_FOLDER} ; zip -qry ${BASE_LOCATION}/${RPI_FINAL_ZIP_NAME} ./*)
